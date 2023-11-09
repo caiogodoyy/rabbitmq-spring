20afae2e074e59ecbecc6afca4bd515624da0b0e
caiogodoyy <caio.godoy.vg@gmail.com>
2023-11-09 17:24:31 -0300

    building spring project

---
+++ src/test/java/com/example/rabbitmqspring/RabbitmqSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
